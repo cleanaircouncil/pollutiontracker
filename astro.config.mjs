38d9c1d84f55633203b45c61e92399ff0859510f
Dain Saint <dain@cipherprime.com>
2025-08-01 15:45:31 -0400

    CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://cleanaircouncil.github.io',
+  base: 'pollutiontracker',
   build: {
     inlineStylesheets: 'never'
   }
