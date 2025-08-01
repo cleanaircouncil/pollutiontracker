a8258faaa4d399ada44996799bb22da98cc4c4f3
Dain Saint <dain@cipherprime.com>
2025-08-01 15:49:07 -0400

    dont need build folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://cleanaircouncil.github.io',
-  base: 'pollutiontracker',
   build: {
     inlineStylesheets: 'never'
   }
